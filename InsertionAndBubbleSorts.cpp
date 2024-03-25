45fc51f45c0a2771d56ba8834f7a9e1144427a55
Gamal-prog <124503906+Gamal-prog@users.noreply.github.com>
2024-03-26 02:32:13 +0600

    Update InsertionAndBubbleSorts.cpp
    A Little Change

--- InsertionAndBubbleSorts.cpp
+++ InsertionAndBubbleSorts.cpp
@@ -25,14 +25,6 @@ int partition(std::vector<int>& arr, int low, int high) {
     return i + 1;
 }
 
-void quickSort(std::vector<int>& arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
 void bubbleSort(std::vector<int>& arr, int n)
 {
     int i, j;
